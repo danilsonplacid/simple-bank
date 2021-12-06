2e0115e25397bac2575a9fe46b0d959d31353283
danilsonplacid <denilsonplacid@gmail.com>
2021-12-07 00:30:06 +0600

    pass

--- js/web.js
+++ js/web.js
@@ -7,7 +7,7 @@ document.getElementById('login-btn').addEventListener('click', function () {
     const passField = document.getElementById('user-pass');
     const usrPass = passField.value;
 
-    if (usrEmail == 'devplacid@web.com' && usrPass  == '32508749') {
+    if (usrEmail == 'devplacid@web.com' && usrPass  == '3250') {
          console.log('Working Working Working');
     //  window.location.href="https://www.google.com/";  
      window.location.href="bank.html";  
