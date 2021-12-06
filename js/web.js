69da0b92cef8a23b375bf1be3c33656af4633a05
danilsonplacid <denilsonplacid@gmail.com>
2021-12-07 00:17:23 +0600

    js file added

---
+++ js/web.js
@@ -0,0 +1,21 @@
+document.getElementById('login-btn').addEventListener('click', function () {
+    const emailField = document.getElementById('user-email');
+    const usrEmail = emailField.value;
+
+    // get ur password
+
+    const passField = document.getElementById('user-pass');
+    const usrPass = passField.value;
+
+    if (usrEmail == 'devplacid@web.com' && usrPass  == '32508749') {
+         console.log('Working Working Working');
+    //  window.location.href="https://www.google.com/";  
+     window.location.href="bank.html";  
+             
+    }
+
+    else{
+        console.log(alert('Error Info')); 
+    }
+    
+})
